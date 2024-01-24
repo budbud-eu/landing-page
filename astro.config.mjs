628dfefff043bfacdc532067e023ede757c0db27
Lucas Brito <jlucasbrito1@gmail.com>
2024-01-24 21:21:21 +0000

    BBH-32: Setup domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://budbud-eu.github.io',
+  site: 'https://budbud.eu',
   integrations: [tailwind()]
 });
