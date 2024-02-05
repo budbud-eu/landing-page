9bf7f5cfead07b30f2a75b9fbbf52b83d5cfb023
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-05 18:13:22 +0000

    Add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://budbud.eu',
-  integrations: [tailwind()]
+  integrations: [tailwind(), svelte()]
 });
