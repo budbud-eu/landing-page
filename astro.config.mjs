a2b8741c93fb2f47045316087a33d016779f8cab
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-09 17:46:29 +0000

    Remove vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-import vercel from "@astrojs/vercel/serverless";
-import * as crypto from 'crypto';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: "hybrid",
-  adapter: vercel()
+  output: 'hybrid'
 });
