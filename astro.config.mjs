23593c95ce3eb884d3eacf388a2429c6e8468256
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-09 17:29:46 +0000

    Import crypto

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
+import * as crypto from 'crypto';
 
 // https://astro.build/config
 export default defineConfig({
