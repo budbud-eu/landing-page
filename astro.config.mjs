181e7f7371ce69e9892fb985e0cac93efdc32605
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-09 00:44:24 +0000

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,11 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://budbud.eu',
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  redirects: {
+    '/': {
+      status: 302,
+      destination: '/maintenance',
+    },
+  }
 });
