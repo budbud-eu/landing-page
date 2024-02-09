2c170c75a372cad9289a26220d939c56a31a972e
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-09 17:52:17 +0000

    Add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: node({
+    mode: "middleware"
+  })
 });
