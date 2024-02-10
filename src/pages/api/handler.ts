5ce5d8fa07310ab28bf33f89e690488a2fdb5f0a
Lucas Brito <jlucasbrito1@gmail.com>
2024-02-10 02:35:42 +0000

    Fix maintenance

--- src/pages/api/handler.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(
-    request: VercelRequest,
-    response: VercelResponse,
-) {
-    // Use 308 for a permanent redirect, 307 for a temporary redirect
-    return response.redirect(307, '/new-route');
-}
